416c6e36ac4f8ec4fb0b85f05f749a3ab3a35f48
leo <coderleo42+leocodes@gmail.com>
2020-06-05 22:37:30 -0400

    ApplyScript: clarify descriptions for MakeBackup and GenerateBins

--- YAMLDatabase/ApplyScriptOptions.cs
+++ YAMLDatabase/ApplyScriptOptions.cs
@@ -11,11 +11,11 @@ namespace YAMLDatabase
         [UsedImplicitly]
         public string ModScriptPath { get; set; }
 
-        [Option("backup", HelpText = "Whether a YML backup should be made before saving the new database")]
+        [Option("backup", HelpText = "Whether a YML backup should be made before saving the new database. Enabled by default.")]
         [UsedImplicitly]
         public bool MakeBackup { get; set; } = true;
 
-        [Option("pack", HelpText = "Whether new bin files should be generated after applying the script")]
+        [Option("pack", HelpText = "Whether new bin files should be generated after applying the script. Enabled by default.")]
         [UsedImplicitly]
         public bool GenerateBins { get; set; } = true;
     }
