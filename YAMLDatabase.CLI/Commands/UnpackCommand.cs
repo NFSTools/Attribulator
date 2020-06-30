f2a07a03ee81ad69adb4b5e87b3cf9273d89770b
leo <coderleo42+leocodes@gmail.com>
2020-06-30 19:31:41 -0400

    get rid of CoreCommands plugin and old YAMLDatabase.Core project

--- YAMLDatabase.CLI/Commands/UnpackCommand.cs
+++ YAMLDatabase.CLI/Commands/UnpackCommand.cs
@@ -1,30 +1,32 @@
 ﻿using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
 using CommandLine;
 using JetBrains.Annotations;
 using Microsoft.Extensions.DependencyInjection;
 using VaultLib.Core.DB;
-using YAMLDatabase.API.Exceptions;
 using YAMLDatabase.API.Plugin;
 using YAMLDatabase.API.Services;
 
-namespace YAMLDatabase.Plugins.CoreCommands
+namespace YAMLDatabase.CLI.Commands
 {
-    [Verb("pack", HelpText = "Packs a database to BIN files.")]
-    public class PackCommand : BaseCommand
+    [Verb("unpack", HelpText = "Unpacks binary VLT files.")]
+    public class UnpackCommand : BaseCommand
     {
-        [Option('i', HelpText = "Directory to read unpacked files from", Required = true)]
+        [Option('i', HelpText = "Directory to read BIN files from", Required = true)]
         [UsedImplicitly]
         public string InputDirectory { get; set; }
 
-        [Option('o', HelpText = "Directory to write BIN files to", Required = true)]
+        [Option('o', HelpText = "Directory to write unpacked files to", Required = true)]
         [UsedImplicitly]
         public string OutputDirectory { get; set; }
 
         [Option('p', HelpText = "The profile to use", Required = true)]
         [UsedImplicitly]
         public string ProfileName { get; set; }
+
+        [Option('f', HelpText = "The format to use", Required = true)]
+        [UsedImplicitly]
+        public string StorageFormatName { get; set; }
 
         public override Task<int> Execute()
         {
@@ -35,16 +37,13 @@ namespace YAMLDatabase.Plugins.CoreCommands
             if (!Directory.Exists(OutputDirectory)) Directory.CreateDirectory(OutputDirectory);
 
             var profile = ServiceProvider.GetRequiredService<IProfileService>().GetProfile(ProfileName);
-            var storageFormatService = ServiceProvider.GetRequiredService<IStorageFormatService>();
-            var storageFormat = storageFormatService.GetStorageFormats()
-                .FirstOrDefault(testStorageFormat => testStorageFormat.CanDeserializeFrom(InputDirectory));
-
-            if (storageFormat == null)
-                return Task.FromException<int>(new CommandException(
-                    $"Cannot find storage format that is compatible with directory [{InputDirectory}]."));
-
+            var storageFormat = ServiceProvider.GetRequiredService<IStorageFormatService>()
+                .GetStorageFormat(StorageFormatName);
             var database = new Database(new DatabaseOptions(profile.GetGameId(), profile.GetDatabaseType()));
-            var serializedDatabaseInfo = storageFormat.Deserialize(InputDirectory, database);
+            var files = profile.LoadFiles(database, InputDirectory);
+            database.CompleteLoad();
+
+            storageFormat.Serialize(database, OutputDirectory, files);
 
             return Task.FromResult(0);
         }
