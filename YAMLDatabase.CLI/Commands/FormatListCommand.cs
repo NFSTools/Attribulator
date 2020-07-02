c72acd776748baf3562464a012fabe206adbf670
leo <coderleo42+leocodes@gmail.com>
2020-07-01 22:18:44 -0400

    add proper logging to FormatListCommand and ProfileListCommand

--- YAMLDatabase.CLI/Commands/FormatListCommand.cs
+++ YAMLDatabase.CLI/Commands/FormatListCommand.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using CommandLine;
 using JetBrains.Annotations;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using YAMLDatabase.API.Plugin;
 using YAMLDatabase.API.Services;
 
@@ -12,12 +14,25 @@ namespace YAMLDatabase.CLI.Commands
     [UsedImplicitly]
     public class FormatListCommand : BaseCommand
     {
+        private ILogger<FormatListCommand> _logger;
+
+        public override void SetServiceProvider(IServiceProvider serviceProvider)
+        {
+            base.SetServiceProvider(serviceProvider);
+
+            _logger = serviceProvider.GetRequiredService<ILogger<FormatListCommand>>();
+        }
+
         public override Task<int> Execute()
         {
             var storageFormatService = ServiceProvider.GetRequiredService<IStorageFormatService>();
+            var storageFormats = storageFormatService.GetStorageFormats().ToList();
 
-            foreach (var storageFormat in storageFormatService.GetStorageFormats()) 
-                Console.WriteLine("{0} - ID: {1}", storageFormat.GetFormatName(), storageFormat.GetFormatId());
+            _logger.LogInformation("Storage Formats ({NumFormats}):", storageFormats.Count);
+            foreach (var storageFormat in storageFormats)
+                _logger.LogInformation("{Name} - ID: {Id}", storageFormat.GetFormatName(),
+                    storageFormat.GetFormatId());
+
             return Task.FromResult(0);
         }
     }
--- YAMLDatabase.CLI/Commands/ProfileListCommand.cs
+++ YAMLDatabase.CLI/Commands/ProfileListCommand.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using CommandLine;
 using JetBrains.Annotations;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using YAMLDatabase.API.Plugin;
 using YAMLDatabase.API.Services;
 
@@ -12,12 +14,24 @@ namespace YAMLDatabase.CLI.Commands
     [UsedImplicitly]
     public class ProfileListCommand : BaseCommand
     {
+        private ILogger<ProfileListCommand> _logger;
+
+        public override void SetServiceProvider(IServiceProvider serviceProvider)
+        {
+            base.SetServiceProvider(serviceProvider);
+
+            _logger = serviceProvider.GetRequiredService<ILogger<ProfileListCommand>>();
+        }
+
         public override Task<int> Execute()
         {
             var profileService = ServiceProvider.GetRequiredService<IProfileService>();
+            var profiles = profileService.GetProfiles().ToList();
 
-            foreach (var profile in profileService.GetProfiles())
-                Console.WriteLine("{0} - ID: {1}", profile.GetName(), profile.GetGameId());
+            _logger.LogInformation("Profiles ({NumProfiles}):", profiles.Count);
+            foreach (var profile in profiles)
+                _logger.LogInformation("{Name} - ID: {Id}; DB Type: {DbType}", profile.GetName(),
+                    profile.GetGameId(), profile.GetDatabaseType());
 
             return Task.FromResult(0);
         }
