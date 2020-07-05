2d43b11e9b8c008c306b72418bd1f4e482e0b687
leo <coderleo42+leocodes@gmail.com>
2020-07-05 13:26:27 -0400

    prepare project for beta release

--- YAMLDatabase.Plugins.SpeedProfiles/SpeedProfilesPlugin.cs
+++ YAMLDatabase.Plugins.SpeedProfiles/SpeedProfilesPlugin.cs
@@ -1,4 +1,12 @@
-﻿using YAMLDatabase.API.Plugin;
+﻿using System.IO;
+using System.Reflection;
+using VaultLib.Core.Hashing;
+using YAMLDatabase.API.Plugin;
+using CarbonModule = VaultLib.Support.Carbon;
+using MostWantedModule = VaultLib.Support.MostWanted;
+using ProStreetModule = VaultLib.Support.ProStreet;
+using UndercoverModule = VaultLib.Support.Undercover;
+using WorldModule = VaultLib.Support.World;
 
 namespace YAMLDatabase.Plugins.SpeedProfiles
 {
@@ -11,7 +19,14 @@ namespace YAMLDatabase.Plugins.SpeedProfiles
 
         public void Init()
         {
-            //
+            HashManager.LoadDictionary(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
+                "Resources", "hashes.txt"));
+
+            new CarbonModule.ModuleDef().Load();
+            new MostWantedModule.ModuleDef().Load();
+            new ProStreetModule.ModuleDef().Load();
+            new UndercoverModule.ModuleDef().Load();
+            new WorldModule.ModuleDef().Load();
         }
     }
 }
