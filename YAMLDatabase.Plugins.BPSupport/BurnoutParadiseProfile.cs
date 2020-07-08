e064a599200ed0739515ccd0cd35b01d80a37fd9
leo <coderleo42+leocodes@gmail.com>
2020-07-08 00:36:39 -0400

    add very rudimentary Burnout Paradise plugin

---
+++ YAMLDatabase.Plugins.BPSupport/BurnoutParadiseProfile.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using VaultLib.Core.DB;
+using VaultLib.Core.Pack;
+using YAMLDatabase.API;
+using YAMLDatabase.API.Data;
+
+namespace YAMLDatabase.Plugins.BPSupport
+{
+    public class BurnoutParadiseProfile : IProfile
+    {
+        public IEnumerable<LoadedFile> LoadFiles(Database database, string directory)
+        {
+            var filesToLoad = Directory.GetFiles(directory, "*.bin", SearchOption.TopDirectoryOnly)
+                .Where(f => !Path.GetFileNameWithoutExtension(f).Equals("schema"))
+                .ToList();
+            filesToLoad.Insert(0, Path.Combine(directory, "schema.bin"));
+
+            return (from file in filesToLoad
+                let vaultPack = new BurnoutVaultPack(Path.GetFileNameWithoutExtension(file))
+                let br = new BinaryReader(File.OpenRead(file))
+                let vaults = vaultPack.Load(br, database, new PackLoadingOptions())
+                select new LoadedFile(Path.GetFileNameWithoutExtension(file), "main", vaults)).ToList();
+        }
+
+        public void SaveFiles(Database database, string directory, IEnumerable<LoadedFile> files)
+        {
+            foreach (var file in files)
+            {
+                Directory.CreateDirectory(Path.Combine(directory, file.Group));
+                IVaultPack vaultPack = new BurnoutVaultPack(file.Name);
+                using var fs = new FileStream(Path.Combine(directory, file.Group, file.Name + ".bin"),
+                    FileMode.Create, FileAccess.ReadWrite);
+                using var bw = new BinaryWriter(fs);
+                vaultPack.Save(bw, file.Vaults.ToList(), new PackSavingOptions());
+            }
+        }
+
+        public string GetName()
+        {
+            return "Burnout Paradise";
+        }
+
+        public string GetGameId()
+        {
+            return "BURNOUT_PARADISE";
+        }
+
+        public DatabaseType GetDatabaseType()
+        {
+            return DatabaseType.X64Database;
+        }
+    }
+}
---
+++ YAMLDatabase.Plugins.BPSupport/BurnoutVaultPack.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using CoreLibraries.IO;
+using VaultLib.Core;
+using VaultLib.Core.DB;
+using VaultLib.Core.Pack;
+
+namespace YAMLDatabase.Plugins.BPSupport
+{
+    public class BurnoutVaultPack : IVaultPack
+    {
+        private readonly string _vaultName;
+
+        public BurnoutVaultPack(string vaultName)
+        {
+            _vaultName = vaultName;
+        }
+
+        public IList<Vault> Load(BinaryReader br, Database database, PackLoadingOptions loadingOptions = null)
+        {
+            var vltOffset = br.ReadUInt32();
+            var vltSize = br.ReadUInt32();
+            var binOffset = br.ReadUInt32();
+            var binSize = br.ReadUInt32();
+
+            if (vltOffset > br.BaseStream.Length)
+                throw new InvalidDataException();
+
+            if (binOffset > br.BaseStream.Length)
+                throw new InvalidDataException();
+
+            br.BaseStream.Position = vltOffset;
+            var vltData = new byte[vltSize];
+
+            if (br.Read(vltData) != vltData.Length) throw new InvalidDataException();
+
+            br.BaseStream.Position = binOffset;
+            var binData = new byte[binSize];
+
+            if (br.Read(binData) != binData.Length) throw new InvalidDataException();
+
+            var vault = new Vault(_vaultName)
+            {
+                BinStream = new MemoryStream(binData),
+                VltStream = new MemoryStream(vltData)
+            };
+
+            using (var loadingWrapper = new VaultLoadingWrapper(vault, loadingOptions?.ByteOrder ?? ByteOrder.Little))
+            {
+                database.LoadVault(vault, loadingWrapper);
+            }
+
+            return new ReadOnlyCollection<Vault>(new List<Vault>(new[] {vault}));
+        }
+
+        public void Save(BinaryWriter bw, IList<Vault> vaults, PackSavingOptions savingOptions)
+        {
+            bw.Write(0x10);
+            var vault = vaults[0];
+            var vw = new VaultWriter(vault, new VaultSaveOptions {HashMode = VaultHashMode.Hash64});
+            var streamInfo = vw.BuildVault();
+            bw.Write((uint) streamInfo.VltStream.Length);
+            bw.Write(0);
+            bw.Write((uint) streamInfo.BinStream.Length);
+
+            streamInfo.VltStream.CopyTo(bw.BaseStream);
+            bw.AlignWriter(0x10);
+            var binOffset = bw.BaseStream.Position;
+            streamInfo.BinStream.CopyTo(bw.BaseStream);
+            var endOffset = bw.BaseStream.Position;
+
+            bw.BaseStream.Position = 8;
+            bw.Write((uint) binOffset);
+            bw.BaseStream.Position = endOffset;
+        }
+    }
+}
